666b1fa0cbd4584c1a3ef49b201212ca30915af8
gaoxianjun <gaoxianjun@gaoxianjundeMacBook-Pro.local>
2015-01-19 20:08:20 +0800

    add sqlprocess test

---
+++ src/main/java/SqlProcess.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gaoxianjun on 15/1/19.
+ */
+public class SqlProcess {
+    public SqlProcess() {
+    }
+    public void dosql(){
+        System.out.println("print mysql");
+    }
+}
---
+++ src/test/java/testmain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by gaoxianjun on 15/1/19.
+ */
+public class testmain {
+    public static void main(String[] args) {
+     SqlProcess SqlPro = new SqlProcess();
+        SqlPro.dosql();
+    }
+}
